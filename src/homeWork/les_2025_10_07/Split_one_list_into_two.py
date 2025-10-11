aba495e12305f83f933ed3040cbea070b16e900d
DevYevheniia <intelligent4nail.art@gmail.com>
2025-10-11 13:26:22 +0300

    add new DZ 3.3. Split one list into two lists

--- src/homeWork/les_2025_10_07/Split_one_list_into_two.py
+++ src/homeWork/les_2025_10_07/Split_one_list_into_two.py
@@ -1,7 +1,14 @@
 #DZ 3.3. Split one list into two lists
 
-original_list = [1, 2, 3, 4, 5, 6]
-copy_original_list_1 = original_list[:3]
-copy_original_list_2 = original_list[3:]
-print(copy_original_list_1)
-print(copy_original_list_2)
+original_list = [1, 2, 3, 4, 5]
+
+if not original_list:
+    result = [[], []]
+elif len(original_list) % 2 == 0:
+    finding_middle_list = len(original_list) // 2
+    result = [original_list[:finding_middle_list], original_list[finding_middle_list:]]
+else:
+    finding_middle_list = len(original_list) // 2 + 1
+    result = [original_list[:finding_middle_list], original_list[finding_middle_list:]]
+
+print(f"{result}")
