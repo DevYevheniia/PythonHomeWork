2cfdef11d42ce0660dc1934f0f7a68942e556fb9
DevYevheniia <intelligent4nail.art@gmail.com>
2025-10-17 13:48:02 +0300

    add correction DZ 5.3. hashtag

--- src/homeWork/les_2025_10_14/variable_name.py
+++ src/homeWork/les_2025_10_14/variable_name.py
@@ -1,12 +1,14 @@
 #DZ 5.1. Variable name
 import keyword
+import string
 
 user_input = input("Enter the string: ")
 if(
     user_input.isidentifier()
     and not keyword.iskeyword(user_input)
     and user_input.islower()
-    and user_input.count('_') <= 1
+    and not any(ch in string.punctuation.replace("_", "") for ch in user_input)
+    and "__" not in user_input
 ):
     print(True)
 else:
